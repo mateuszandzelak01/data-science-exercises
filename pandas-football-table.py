6cf3badfdd218d14e16417ee064c6c9f41030d6f
mateuszandzelak01 <m.andzelak@gmail.com>
2019-05-14 20:44:40 +0200

    p_league done

--- pandas-football-table.py
+++ pandas-football-table.py
@@ -8,7 +8,16 @@ print(p_league.head(6))
 
 p_league[['F','A', 'P', 'W', 'D', 'L', 'GD', 'Pts']] = p_league[['F','A', 'P', 'W', 'D', 'L', 'GD', 'Pts']].apply(pd.to_numeric, errors='coerce')
 
-p_league.drop(p_league.tail(1).index)
+p_league = p_league.drop(p_league.tail(1).index)
 p_league.rename(columns={'Unnamed: 0':'Position'}, inplace=True)
 
+p_league['Goal Ratio'] = round(p_league['F']/p_league['A'], 1)
+p_league = p_league[['Position','Team', 'P', 'W', 'D', 'L','F','A','Goal Ratio','GD','Pts']]
 
+p_league['Goals/game'] = round((p_league.F / p_league.P), 1)
+p_league['Goals conceded/game'] = round((p_league['A'] / p_league['P']), 1)
+print(p_league.sort_values(['Goals/game', 'Goal Ratio']).head(5))
+
+goal_differnce = p_league.loc[17, 'GD'] - p_league.loc[16, 'GD']
+
+print('The goal difference between Cardiff and Brighton is ' + str(goal_differnce))
